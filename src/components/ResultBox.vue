5cbad2aac715b029e6d3e561df3fe1ac6ff89158
mateusz2247 <mateusz2247@gmail.com>
2023-04-11 22:27:46 +0200

    po dodaniu router, nie dziala to:, zrobienie cwiczenie z routerlinki i compued colors

--- src/components/ResultBox.vue
+++ src/components/ResultBox.vue
@@ -1,110 +1,115 @@
 <template>
-  <div>
+	<div>
+		<!-- text between -->
+		<p v-text="'And the result...'" />
 
-    <!-- text between -->
-    <p
-      v-text="'And the result...'" />
-
-    <!-- mixture effect -->
-    <!-- <big-mixture-item
+		<!-- mixture effect -->
+		<!-- <big-mixture-item
       :color="mixtureEffectFill" /> -->
-     <!-- <flask-item
+		<!-- <flask-item
         :key="index"
         v-for="(mixture, index) in mixtures"
         :variant="mixture.variant"
         :amount="mixture.amount"
         @increment="$emit('increment', index)"
         @decrement="$emit('decrement', index)" /> -->
-        <FlaskItem
-        style="margin: 3rem auto"
-        :amount="100"
-        :buttonsVisible="false"
-        :size="15"
-        :color="mixtureEffectFill"
+		<FlaskItem
+			style="margin: 3rem auto"
+			:amount="100"
+			:buttonsVisible="false"
+			:size="15"
+			:color="mixtureEffectFill" />
+		<p v-html="mixtureEffectFill"></p>
+		<button-item
+			@click="$emit('refresh')"
+			:size="4"
+			:movement="-0.5"
+			:font-size="1.5"
+			icon="sync" />
+		<button-item
+			@click="modalVisible = true"
+			:size="4"
+			:movement="-0.5"
+			:font-size="1.5"
+			icon="question" />
 
-        />
-    <button-item
-      @click="$emit('refresh')"
-      :size="4"
-      :movement="-0.5"
-      :font-size="1.5"
-      icon="sync" />
-      <button-item
-      @click="modalVisible = true"
-      :size="4"
-      :movement="-0.5"
-      :font-size="1.5"
-      icon="book" />
-    <!-- <button
-      @click="$emit('refresh')"
-      class="refresh-btn">
-      <span
-        class="fas fa-sync" />
-    </button> -->
-    <ModalItem
-  v-if="modalVisible"
-  @cancel="modalVisible = false">
-   <template v-slot:header>
-    About the app
-   </template>
+		<RouterLink :to="createLink">
+			<button-item
+				:size="4"
+				:movement="-0.5"
+				:font-size="1.5"
+				icon="share-alt" />
+		</RouterLink>
 
-   <template v-slot:body>
-    Mix three colors to create the perfect one!
-   </template>
+		<ModalItem v-if="modalVisible" @cancel="modalVisible = false">
+			<template v-slot:header> About the app </template>
 
-   <template v-slot:footer>
-    <button-item icon="pi pi-fw pi-check" />
-   </template>
+			<template v-slot:body>
+				Mix three colors to create the perfect one!
+			</template>
 
-</ModalItem>
-  </div>
+			<template v-slot:footer>
+				<button-item icon="pi pi-fw pi-check" />
+			</template>
+		</ModalItem>
+	</div>
 </template>
 
 <script>
 /* import BigMixtureItem from './BigMixtureItem' */
-import ModalItem from './ModalItem.vue'
-import FlaskItem from './FlaskItem.vue'
-import ButtonItem from './shared/ButtonItem.vue'
+import ModalItem from "./ModalItem.vue";
+import FlaskItem from "./FlaskItem.vue";
+import ButtonItem from "./shared/ButtonItem.vue";
 export default {
-  data: () => ({ modalVisible: false }),
-  name: 'ResultsBox',
-  props: {
-    mixtures: {
-      type: Array,
-      required: true
-    }
-  },
-  computed: {
-    mixtureEffectFill () {
-      const [redCol, greenCol, blueCol] = this.mixtures.map(item => Math.floor(item.amount * 2.5))
-      return `rgb(${redCol}, ${greenCol}, ${blueCol})`
-    }
-  },
-  components: {
-    ButtonItem, FlaskItem, ModalItem
-  }
-}
-
+	data: () => ({ modalVisible: false }),
+	name: "ResultsBox",
+	props: {
+		mixtures: {
+			type: Array,
+			required: true,
+		},
+	},
+	computed: {
+		mixtureEffectFill() {
+			const [redCol, greenCol, blueCol] = this.mixtures.map((item) =>
+				Math.floor(item.amount * 2.5)
+			);
+			return `rgb(${redCol}, ${greenCol}, ${blueCol})`;
+		},
+		createLink() {
+			const [redCol, greenCol, blueCol] = this.mixtures.map((item) =>
+				Math.floor(item.amount * 2.5)
+			);
+			console.log(`color/${redCol}/${greenCol}/${blueCol}`);
+			return `color/${redCol}/${greenCol}/${blueCol}`;
+		},
+	},
+	components: {
+		ButtonItem,
+		FlaskItem,
+		ModalItem,
+	},
+};
 </script>
 
 <style scoped lang="scss">
 .refresh-btn {
-  background-color: #9a9a9a;
-  background-image: linear-gradient(0deg, #9a9a9a 0%, #e8fdff 100%);
-  width: 4rem;
-  height: 4rem;
-  border: none;
-  border-radius: 50%;
-  cursor: pointer;
-  -webkit-box-shadow: 0 20px 40px 0 rgba(107,154,212,.1);
-  box-shadow: 0 20px 40px 0 rgba(107,154,212,.1);
-  transition: .3s;
-  outline: none;
-  font-size: 1.5rem;
-  color: #637892;
+	background-color: #9a9a9a;
+	background-image: linear-gradient(0deg, #9a9a9a 0%, #e8fdff 100%);
+	width: 4rem;
+	height: 4rem;
+	border: none;
+	border-radius: 50%;
+	cursor: pointer;
+	-webkit-box-shadow: 0 20px 40px 0 rgba(107, 154, 212, 0.1);
+	box-shadow: 0 20px 40px 0 rgba(107, 154, 212, 0.1);
+	transition: 0.3s;
+	outline: none;
+	font-size: 1.5rem;
+	color: #637892;
 
-  &:hover {
-    margin-top: -0.5rem;
-  }
+	&:hover {
+		margin-top: -0.5rem;
+	}
 }
 </style>
