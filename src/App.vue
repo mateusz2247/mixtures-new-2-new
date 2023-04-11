dc5469114d2736da85a1eddcb62a792056fd45bd
mateusz2247 <mateusz2247@gmail.com>
2023-04-11 15:05:29 +0200

    dodana nawigacja

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,66 @@
 <template>
-  <div id="app">
-    <div class="container">
+	<div id="app">
+		<div class="container">
+			
 
-      <!-- header -->
-      <app-header />
+			<!-- header -->
+			<app-header />
+			<div class="card">
+				<TabMenu
+					style="
+						border-radius: 1rem;
+						box-shadow: 0 20px 40px 0 rgba(107, 154, 212, 0.3);
+						overflow: hidden;
+					"
+					:model="items" />
+				<!-- <router-view /> -->
+			</div>
 
-      <!-- color mixin -->
-      <color-mixin />
-
-    </div>
-  </div>
+			<!-- color mixin -->
+			<color-mixin />
+		</div>
+	</div>
 </template>
 
 <script>
-import './styles/global.scss'
-import AppHeader from './components/AppHeader'
-import ColorMixin from './components/ColorMixin'
+import "./styles/global.scss";
+import AppHeader from "./components/AppHeader";
+import ColorMixin from "./components/ColorMixin";
+import TabMenu from "primevue/tabmenu";
 
 export default {
-  name: 'App',
-  components: {
-    AppHeader,
-    ColorMixin
-  }
-}
+	name: "App",
+	components: {
+		AppHeader,
+		ColorMixin,
+		TabMenu,
+	},
+	data() {
+		return {
+			items: [
+				{
+					label: "Home",
+					icon: "pi pi-fw pi-home",
+				},
+				{
+					label: "Calendar",
+					icon: "pi pi-fw pi-filter",
+				},
+				{
+					label: "Edit",
+					icon: "pi pi-fw pi-info",
+				},
+			],
+		};
+	},
+};
 </script>
 
 <style lang="scss">
 .container {
-  max-width: 800px;
-  margin: 0 auto;
-  text-align: center;
-  padding: 2rem;
+	max-width: 800px;
+	margin: 0 auto;
+	text-align: center;
+	padding: 2rem;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,17 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
+createApp(App).mount("#app");
 
-createApp(App).mount('#app')
+import PrimeVue from 'primevue/config';
+
+const app = createApp(App);
+app.use(PrimeVue);
+//theme
+import "primevue/resources/themes/lara-light-indigo/theme.css";     
+    
+//core
+import "primevue/resources/primevue.min.css";
+
+//icons
+import "primeicons/primeicons.css";
+
