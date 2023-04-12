3ddf2fd70042ed93b3d9bde613e71cf66a824153
mateusz2247 <mateusz2247@gmail.com>
2023-04-12 07:52:08 +0200

    animacje nie dzialaja

--- src/components/FlaskItem.vue
+++ src/components/FlaskItem.vue
@@ -1,133 +1,184 @@
 <template>
-  <div class="flask" :style="flaskStyle">
-    <!-- decrement btn -->
-    <ButtonItem
-      v-if="buttonsVisible"
-      class="flask__btn flask__btn--left"
-      icon="arrow-down"
-      @click="$emit('decrement')"
-    />
-    <div :class="fillClasses" :style="fillStyle" />
-    <!-- increment btn -->
-    <ButtonItem
-      v-if="buttonsVisible"
-      class="flask__btn flask__btn--right"
-      icon="arrow-up"
-      :movement="-0.5"
-      @click="$emit('increment')"
-    />
-  </div>
+	<div ref="flk" class="flask fadeIn" :style="flaskStyle">
+		<!-- decrement btn -->
+		<ButtonItem
+			v-if="buttonsVisible"
+			class="flask__btn flask__btn--left"
+			icon="arrow-down"
+			@click="
+				flkMethod();
+				$emit('decrement');
+			" />
+		<div :class="fillClasses" :style="fillStyle" />
+		<!-- increment btn -->
+		<ButtonItem
+			v-if="buttonsVisible"
+			class="flask__btn flask__btn--right"
+			icon="arrow-up"
+			:movement="-0.5"
+			@click="
+				flkMethod();
+				$emit('increment');
+			" />
+	</div>
 </template>
 
 <script>
-import ButtonItem from './shared/ButtonItem.vue'
+import ButtonItem from "./shared/ButtonItem.vue";
 export default {
-  name: 'FlaskItem',
-  props: {
-    size: {
-      type: Number,
-      default: 10
-    },
-    amount: {
-      type: Number,
-      default: 50
-    },
-    color: {
-      type: String
-    },
-    variant: {
-      type: String
-    },
-    buttonsVisible: {
-      type: Boolean,
-      default: true
-    }
-  },
-  components: {
-    ButtonItem
-  },
-  computed: {
-    flaskStyle () {
-      return {
-        height: `${this.size}rem`,
-        width: `${this.size}rem`
-      }
-    },
+	name: "FlaskItem",
+	props: {
+		size: {
+			type: Number,
+			default: 10,
+		},
+		amount: {
+			type: Number,
+			default: 50,
+		},
+		color: {
+			type: String,
+		},
+		variant: {
+			type: String,
+		},
+		buttonsVisible: {
+			type: Boolean,
+			default: true,
+		},
+	},
+	components: {
+		ButtonItem,
+	},
 
-    fillClasses () {
-      return ['flask__fill', this.variant && `flask__fill--${this.variant}`]
-    },
+	methods: {
+		flkMethod() {
+			this.$refs.flk.classList.add("zoomIn");
+      setTimeout (setTimeout(() => { this.$refs.flk.classList.remove("zoomIn") }, 300))
+      console.log("wykonalem funkcje")
 
-    fillStyle () {
-      const style = { height: this.amount + '%' }
+		},
+	},
 
-      if (!this.variant) {
-        style.backgroundColor = this.color || '#fff'
-      }
+	computed: {
+		flaskStyle() {
+			return {
+				height: `${this.size}rem`,
+				width: `${this.size}rem`,
+			};
+		},
 
-      return style
-    }
-  }
-}
+		fillClasses() {
+			return ["flask__fill", this.variant && `flask__fill--${this.variant}`];
+		},
+
+		fillStyle() {
+			const style = { height: this.amount + "%" };
+
+			if (!this.variant) {
+				style.backgroundColor = this.color || "#fff";
+			}
+
+			return style;
+		},
+	},
+};
 </script>
 
 <style lang="scss">
+
 .flask {
-  display: block;
-  border: 10px solid #ddd;
-  border-radius: 50%;
-  margin: 2rem 1rem;
-  position: relative;
-  display: flex;
-  align-items: flex-end;
-  overflow: hidden;
-  box-shadow: 0 20px 40px 0 rgba(107, 154, 212, 0.3);
+	display: block;
+	border: 10px solid #ddd;
+	border-radius: 50%;
+	margin: 2rem 1rem;
+	position: relative;
+	display: flex;
+	align-items: flex-end;
+	overflow: hidden;
+	box-shadow: 0 20px 40px 0 rgba(107, 154, 212, 0.3);
 
-  &__fill {
-    width: 100%;
-    height: 100%;
-    transition: 0.2s;
+	&__fill {
+		width: 100%;
+		height: 100%;
+		transition: 0.2s;
 
-    &--red {
-      background-color: #ff7d3b;
-      background-image: linear-gradient(189deg, #ff7d3b 0%, #ff2525 74%);
-    }
+		&--red {
+			background-color: #ff7d3b;
+			background-image: linear-gradient(189deg, #ff7d3b 0%, #ff2525 74%);
+		}
 
-    &--green {
-      background-color: #2e561c;
-      background-image: linear-gradient(0deg, #2e561c 0%, #2af598 100%);
-    }
+		&--green {
+			background-color: #2e561c;
+			background-image: linear-gradient(0deg, #2e561c 0%, #2af598 100%);
+		}
 
-    &--blue {
-      background-color: #25b6bf;
-      background-image: linear-gradient(183deg, #25b6bf 0%, #0070a2 100%);
-    }
-  }
+		&--blue {
+			background-color: #25b6bf;
+			background-image: linear-gradient(183deg, #25b6bf 0%, #0070a2 100%);
+		}
+	}
 
-  &::before {
-    content: "";
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    background-color: #9a9a9a;
-    background-image: linear-gradient(0deg, #9a9a9a 0%, #e8fdff 100%);
-    opacity: 0.2;
-  }
+	&::before {
+		content: "";
+		position: absolute;
+		top: 0;
+		left: 0;
+		width: 100%;
+		height: 100%;
+		background-color: #9a9a9a;
+		background-image: linear-gradient(0deg, #9a9a9a 0%, #e8fdff 100%);
+		opacity: 0.2;
+	}
 
-  &__btn {
-    position: absolute;
-    top: 50%;
-    transform: translateY(-50%);
+	&__btn {
+		position: absolute;
+		top: 50%;
+		transform: translateY(-50%);
 
-    &--right {
-      right: 1rem;
-    }
+		&--right {
+			right: 1rem;
+		}
 
-    &--left {
-      left: 1rem;
-    }
-  }
+		&--left {
+			left: 1rem;
+		}
+	}
+}
+.fadeIn {
+	animation-name: "fadeIn";
+	animation-iteration-count: 1;
+	animation-duration: 0.3s;
+}
+
+@keyframes fadeIn {
+	0% {
+		opacity: 0;
+	}
+	100% {
+		opacity: 1;
+	}
+}
+
+.zoomIn {
+	animation-name: "zoomIn";
+	animation-iteration-count: 1;
+	animation-duration: 0.3s;
+}
+
+@keyframes zoomIn {
+	0% {
+		opacity: 0.8;
+		transform: scale3d(1.1, 1.1, 1.1);
+	}
+	20% {
+		transform: scale3d(1, 1, 1);
+	}
+	70% {
+		transform: scale3d(1.1, 1.1, 1.1);
+	}
+	100% {
+		opacity: 1;
+	}
 }
 </style>
