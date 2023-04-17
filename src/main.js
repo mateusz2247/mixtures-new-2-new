24ce4c4d333c88114c47c44c978980e18a17853e
mateusz2247 <mateusz2247@gmail.com>
2023-04-17 06:41:27 +0200

    po instalacji vuex

--- src/main.js
+++ src/main.js
@@ -8,17 +8,18 @@ import "primevue/resources/themes/lara-light-indigo/theme.css";
 import "primevue/resources/primevue.min.css";
 import "primeicons/primeicons.css"; */
 
-
-import 'animate.css/animate.min.css'
+import "animate.css/animate.min.css";
 import router from "./router";
 import { createApp } from "vue";
 import App from "./App.vue";
-import PrimeVue from 'primevue/config';
+import PrimeVue from "primevue/config";
 import "primevue/resources/themes/lara-light-indigo/theme.css";
 import "primevue/resources/primevue.min.css";
 import "primeicons/primeicons.css";
+import store from "./store";
 
 const app = createApp(App);
 app.use(router);
 app.mount("#app");
 app.use(PrimeVue);
+app.use(store);
---
+++ src/store/index.js
@@ -0,0 +1,15 @@
+import { createStore } from 'vuex'
+
+export default createStore({
+  state: {
+    colors: [
+      { red: 10, green: 20, blue: 30 },
+      { red: 20, green: 120, blue: 90 }
+    ]
+  },
+  mutations: {
+    ADD_COLOR (state, color) {
+      state.colors.push(color)
+    }
+  }
+})
